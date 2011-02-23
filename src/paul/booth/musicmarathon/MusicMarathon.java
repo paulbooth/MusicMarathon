5d58d199c46f3b866dbbadb15e196dcc5c979eee
Karl Schults <karl.schults@students.olin.edu>
2011-02-23 15:15:18 -0500

    Plays a single song on startup

--- src/paul/booth/musicmarathon/MusicMarathon.java
+++ src/paul/booth/musicmarathon/MusicMarathon.java
@@ -1,18 +1,30 @@
 package paul.booth.musicmarathon;
 
+import java.io.IOException;
 import java.util.List;
+
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.echonest.api.v4.EchoNestAPI;
 import com.echonest.api.v4.EchoNestException;
 import com.echonest.api.v4.Params;
 import com.echonest.api.v4.Song;
 import com.echonest.api.v4.SongParams;
+import com.echonest.api.v4.Track;
 
 import paul.booth.musicmarathon.R;
 import paul.booth.musicmarathon.MusicMarathonView.MusicMarathonGestureListener.MusicMarathonThread;
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MusicMarathon extends Activity {	
 	
@@ -24,7 +36,8 @@ public class MusicMarathon extends Activity {
     
     final static String API_KEY = "SNPTOEM7JQD9EGMF2";
     EchoNestAPI echoNest;
-    Song firstSong;
+    String firstSongUrl;
+    MediaPlayer mp;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,29 +51,63 @@ public class MusicMarathon extends Activity {
         
         
         echoNest = new EchoNestAPI(API_KEY);
-        firstSong = getRandomSong(echoNest);
-        String s = firstSong.getAudio();
-        int x = 4;
-        x +=3;
+        mp = new MediaPlayer();
+        
+        firstSongUrl = getRandomSong();
+        setNewSong(firstSongUrl);
     }
     
-    private Song getRandomSong(EchoNestAPI echoNest) {
-        float songHottness = (float) Math.random();
-        Song song = null;
-        
-        SongParams p = new SongParams();
-        p.setMinSongHotttnesss(songHottness);
-        p.setMaxSongHotttnesss(songHottness);
-        try {
-			List<Song> songs = echoNest.searchSongs(p);
-			song = songs.get(0);
-		} catch (EchoNestException e) {
+    private void setNewSong(String url) {
+    	mp.reset();
+    	try {
+			mp.setDataSource(url);
+	    	mp.prepare();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		return song;
+    	mp.start();
     }
     
-    
+    private String getRandomSong() {
+        float songHottness;
+        String url = null;
+        Song song = null;
+        Track t;
+        String id = null;
+        
+        while (id == null) {
+        	songHottness  = (float) Math.random();
+            SongParams p = new SongParams();
+            p.setMinSongHotttnesss(songHottness);
+            p.addIDSpace("7digital");
+            try {
+    			List<Song> songs = echoNest.searchSongs(p);
+    			if (songs.isEmpty()) {
+    				continue;
+    			}
+    			song = songs.get(0);
+    			t = song.getTrack("7digital");
+    			if (t == null) {
+    				continue;
+    			}
+    			id = t.getForeignID();
+    			if (id == null) {
+    				continue;
+    			}
+    			url = SevenDigitalAPI.getPreviewTrackURL(id);
+            } catch (EchoNestException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}        	
+        }
+		
+		return url;
+    }
 }
